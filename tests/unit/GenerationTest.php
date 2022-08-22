19a39a53382457ac2961b0fa3bc453182f736a06
Artur <0x00.dev@gmail.com>
2022-08-22 17:02:19 +0500

    Update GenerationTest.php

--- tests/unit/GenerationTest.php
+++ tests/unit/GenerationTest.php
@@ -19,6 +19,12 @@ class GenerationTest extends \Codeception\Test\Unit
         $this->assertDirectoryExists("$test_dir/docs/TestComponent", 'Проверяем, что директория TestComponent создана.');
         $this->assertFileExists("$test_dir/docs/TestComponent/GeneratorTestClass.html", 'Проверяем, что документация для GeneratorTest создана.');
         $this->assertFileEquals("$test_dir/docs/index.html", "$test_dir/test_docs/index.html", 'Проверяем, что сгенерированный заглавный файл и эталон одинаковы.');
-        $this->assertFileEquals("$test_dir/docs/TestComponent/GeneratorTestClass.html", "$test_dir/test_docs/TestComponent/GeneratorTestClass.html", 'Проверяем, что сгенерированный файл класса и эталон одинаковы.');
+        $expected_file_descriptor = \fopen("$test_dir/docs/TestComponent/GeneratorTestClass.html",'r');
+        $etalon_file_descriptor = \fopen("$test_dir/test_docs/TestComponent/GeneratorTestClass.html",'r');
+        $expected_file_data = \fread($expected_file_descriptor, \filesize("$test_dir/docs/TestComponent/GeneratorTestClass.html"));
+        $etalon_file_data = \fread($etalon_file_descriptor, \filesize("$test_dir/test_docs/TestComponent/GeneratorTestClass.html"));
+        \fclose($expected_file_descriptor);
+        \fclose($etalon_file_descriptor);
+        $this->assertEquals($expected_file_data, $etalon_file_data, 'Проверяем, что сгенерированный файл класса и эталон одинаковы.');
     }
 }
